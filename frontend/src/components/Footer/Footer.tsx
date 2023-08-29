e3bdcb590680ea9ea94e920b75c9d2e08da83333
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-08-29 10:33:43 -0600

    Add: generated Footer
    	Generated structure of footer and some basic styling.

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className={`flex flex-row p-4 bg-slate-500 text-white`}>
+      Footer example here    
+    </footer>
+  )
+}
+
+export default Footer
